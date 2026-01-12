60bfd5f8be56357636bac424ae87f15d0739d01d
DirtCheapDeeds <huntstake@gmail.com>
2026-01-11 22:09:28 -0600

    add more tests

--- src/tests/comparePdfs.test.ts
+++ src/tests/comparePdfs.test.ts
@@ -14,15 +14,13 @@ describe("comparePdfs", () => {
   });
 
   it("should correclty identify the diffs in two different pdfs", async () => {
-    const file1: Buffer = readFileSync("./src/tests/data/baseline1.pdf");
-    const file2: Buffer = readFileSync(
-      "./src/tests/data/baseline1-with-diffs.pdf",
-    );
+    const file1 = readFileSync("./src/tests/data/baseline1.pdf");
+    const file2 = readFileSync("./src/tests/data/baseline1-with-diffs.pdf");
 
-    const expectedPage1Diff: Buffer = readFileSync(
+    const expectedPage1Diff = readFileSync(
       "./src/tests/data/baseline1-page-1-diff.png",
     );
-    const expectedPage2Diff: Buffer = readFileSync(
+    const expectedPage2Diff = readFileSync(
       "./src/tests/data/baseline1-page-2-diff.png",
     );
 
@@ -35,5 +33,34 @@ describe("comparePdfs", () => {
     expect(diff2?.diffPng.equals(expectedPage2Diff)).toBe(true);
     expect(diffs?.length).toBe(2);
     expect(diffPageCount).toBe(false);
+  });
+
+  it("should consider two pdfs not equal if page counts differ with no other diffs", async () => {
+    const file1 = readFileSync("./src/tests/data/baseline2.pdf");
+    const file2 = readFileSync("./src/tests/data/baseline2-only-page-1.pdf");
+
+    const { equal, diffs, diffPageCount } = await comparePdfs(file1, file2);
+
+    expect(equal).toBe(false);
+    expect(diffs?.length).toBe(0);
+    expect(diffPageCount).toBe(true);
+  });
+
+  it("should consider two pdfs not equal if page counts differ with other diffs", async () => {
+    const file1 = readFileSync("./src/tests/data/baseline2.pdf");
+    const file2 = readFileSync(
+      "./src/tests/data/baseline2-only-page-1-with-diffs.pdf",
+    );
+
+    const expectedDiff = readFileSync("./src/tests/data/baseline2-diff.png");
+
+    const { equal, diffs, diffPageCount } = await comparePdfs(file1, file2);
+
+    const [diff] = diffs ?? [];
+
+    expect(equal).toBe(false);
+    expect(diff?.diffPng.equals(expectedDiff)).toBe(true);
+    expect(diffs?.length).toBe(1);
+    expect(diffPageCount).toBe(true);
   });
 });
